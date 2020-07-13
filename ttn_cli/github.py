ed7d46734b736f985c553af52ad9b296f6576ebc
Amulya <amulyasharma27797@gmail.com>
2020-07-13 15:50:08 +0530

    Added init and delete method

--- ttn_cli/github.py
+++ ttn_cli/github.py
@@ -1,23 +1,36 @@
 # This file contains all the git related methods
 import os
 
-# import subprocess
 from datetime import date, datetime
 
-from git import Repo, InvalidGitRepositoryError
+from git import Repo, InvalidGitRepositoryError, \
+    CheckoutError, GitCommandError
 
 
 class GithubOperations:
+    """
+    This class contains the operations which refers to Git
+    """
     def __init__(self):
         self.path = os.getcwd()
 
         # Checks if repo is initialised or not
         try:
-            self.repo = Repo(self.path)
+            self.repo = Repo('/home/amulya/Desktop/TTN_CLI/ttn-cli/')
             # self.repo = Repo(self.path)
-        except InvalidGitRepositoryError:
-            print("Git Repository not Initialised. "
+        except InvalidGitRepositoryError as err:
+            self.repo = None
+            print(f"Invalid Git Repository '{err}'. "
                   "Make sure you are in the correct directory.")
+
+    def init_repo(self):
+        """
+        This method initialises
+        a new git repo to the given path.
+        :return: Path where git repo is initialised (or) re-initialised
+        """
+        self.repo = Repo.init(self.path)
+        return f"Repo Initialised to path '{self.path}'"
 
     def get_all_branches(self):
         return [branch.name for branch in self.repo.branches]
@@ -52,7 +65,34 @@ class GithubOperations:
                                  f'--author={author}',
                                  f'--after={from_date}')
 
+    def branch_checkout(self, branch_name: str) -> str:
+        """
+        This method checkouts to the given branch
+        else throws the specific error.
+        :param branch_name: Name of the branch
+        :return: Checkouts to given branch (or) throws error if any
+        """
+        try:
+            return self.repo.git.checkout(branch_name)
+        except CheckoutError as err:
+            return err.args[2].decode('utf-8')
+        except GitCommandError as err:
+            return err.args[2].decode('utf-8')
+
+    def delete_local_branch(self, branch_name: str) -> str:
+        """
+        This method deletes the git branch in local
+        and throws an error if branch doesn't exists.
+        :param branch_name: Name of the branch
+        :return: Message if branch is deleted (or) error has occurred.
+        """
+        try:
+            return self.repo.git.branch("-d", branch_name)
+        except GitCommandError as err:
+            return err.args[2].decode()
+
 
 git = GithubOperations()
-print(git.get_commit_message())
-print(git.get_logs(author="vibhor", from_date="2020-07-02"))
+# print(git.get_commit_message())
+# print(git.branch_checkout(branch_name='master'))
+# print(git.get_logs(author="vibhor", from_date="2020-07-02"))
